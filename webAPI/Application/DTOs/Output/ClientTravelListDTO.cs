cb4f9fab73b3e7e2357853dcdd3ffc118014e5e7
Sérgio Henrique Nogueira Ferreira <1221504@isep.ipp.pt>
2023-12-13 17:23:00 +0000

    commit

---
+++ webAPI/Application/DTOs/Output/ClientTravelListDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace webAPI.Application.DTOs.Output
+{
+    public class ClientTravelListDTO
+    {
+        public required string ClientName { get; set; }
+
+        public List<TravelDTO>? Travels { get; set; }
+    }
+}
